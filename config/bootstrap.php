2a6e716ade687b96c4d323d408f6fa0e9fc3b28f
adiDumitru <adrian,dumitru.68@gmail.com>
2018-01-20 19:16:49 +0200

    First commit

---
+++ config/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrian Dumitru
+ * Date: 10/8/2017
+ * Time: 12:56 PM
+ */
+
+var_dump('load bootstrap');
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adrian Dumitru
+ * Date: 1/20/2018
+ * Time: 6:51 PM
+ */
